3986a274d0bac8d805bf076308134b1fe0d23c44
zhanglong <zl123456>
2019-12-01 00:14:29 +0800

    设置超时时间

--- src/main/java/com/component/thread/CustomThreadFactory.java
+++ src/main/java/com/component/thread/CustomThreadFactory.java
@@ -2,6 +2,7 @@ package com.component.thread;
 
 import java.util.Objects;
 import java.util.concurrent.ThreadFactory;
+import java.util.function.Supplier;
 
 /**
  * @Author zhanglong
@@ -10,34 +11,31 @@ import java.util.concurrent.ThreadFactory;
  */
 public class CustomThreadFactory implements ThreadFactory {
 
-    public static final String DEFAULTTHREADGROUPNAME = "DEFAULT-COMPUTER-THREAD-GROUP";
+    private static final String THREAD_GROUP_NAME = "default-worker-task-group";
     /**
      * description 线程组，用来排查线程问题，并且隔离线程数据
      */
     private ThreadGroup threadGroup;
-    /**
-     * description 线程名称
-     */
-    private String threadName;
+
 
     /**
      * description 自定义线程工厂
      */
-    public CustomThreadFactory bindingThreadGroup(ThreadGroup threadGroup, String threadName){
+    public CustomThreadFactory bindingThreadGroup(ThreadGroup threadGroup) {
         this.threadGroup = threadGroup;
-        this.threadName = threadName;
         return this;
     }
+
     /**
      * description 线程工厂创建线程
      */
     @Override
     public Thread newThread(Runnable target) {
         Objects.requireNonNull(target, "runable could not be null");
-        if (Objects.isNull(threadGroup)){
-            threadGroup = new ThreadGroup(DEFAULTTHREADGROUPNAME);
+        if (Objects.isNull(threadGroup)) {
+            threadGroup = new ThreadGroup(THREAD_GROUP_NAME);
         }
-        Thread thread = new Thread(threadGroup, target, threadName);
+        Thread thread = new Thread(threadGroup, target);
         //设置线程的执行级别
         thread.setPriority(Thread.NORM_PRIORITY);
         thread.setUncaughtExceptionHandler(Thread.getDefaultUncaughtExceptionHandler());
--- src/main/java/com/component/thread/worker/WorkTaskQueue.java
+++ src/main/java/com/component/thread/worker/WorkTaskQueue.java
@@ -2,6 +2,8 @@ package com.component.thread.worker;
 
 import com.component.thread.CustomThreadFactory;
 import com.component.thread.ThreadPoolProperties;
+import javafx.util.Pair;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Objects;
 import java.util.Queue;
@@ -16,18 +18,43 @@ import java.util.concurrent.atomic.AtomicInteger;
 /**
  * created by zhanglong and since  2019/11/17  11:09 上午
  *
- * @description: 业务工作队列
+ * @description: 业务工作队列 本身线程不安全；单线程操作，不能用于多线程嵌套使用
  */
+@Slf4j
 public final class WorkTaskQueue {
-
+    /**
+     * description 最大等待时间
+     */
+    private long maxWait = 60000L;
+    /**
+     * description 线程队列
+     */
     private Queue<Runnable> workTaskQueue = new ConcurrentLinkedQueue<>();
 
     private AtomicInteger taskCount = new AtomicInteger(0);
-    private static final String THREAD_GROUP_NAME = "default-sharding-group";
-    private static final String THREAD_NAME = "default-sharding-thread";
 
     /**
-     * description 注册任务
+     * create by ZhangLong on 2019/11/30
+     * description 每个任务超时时间默认值为60000ms
+     */
+    public WorkTaskQueue() {
+    }
+
+    /**
+     * create by ZhangLong on 2019/11/30
+     *
+     * @param maxWait 小于0执行任务没有超时,轮训处理
+     */
+    public WorkTaskQueue(long maxWait) {
+        if (maxWait <= 0) {
+            this.maxWait = Long.MAX_VALUE;
+        } else {
+            this.maxWait = maxWait;
+        }
+    }
+
+    /**
+     * description 注册任务 线程安全同步
      */
     public void register(Runnable task) {
         taskCount.incrementAndGet();
@@ -44,41 +71,56 @@ public final class WorkTaskQueue {
 
     public void submit(ThreadPoolProperties threadPoolProperties) {
         CustomThreadFactory threadFactory = new CustomThreadFactory();
-        threadFactory.bindingThreadGroup(new ThreadGroup(THREAD_GROUP_NAME), THREAD_NAME);
+        threadFactory.bindingThreadGroup(new ThreadGroup(threadPoolProperties.getThreadGroupName()));
         ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
                 threadPoolProperties.getCorePoolSize(),
                 threadPoolProperties.getMaximumPoolSize(),
                 threadPoolProperties.getKeepAliveTime(),
                 TimeUnit.SECONDS, threadPoolProperties.getWorkQuezue(), threadFactory);
         CountDownLatch countDownLatch = new CountDownLatch(taskCount.get());
-        Queue<FutureTask<Object>> workingTaskQueue = new ArrayBlockingQueue<>(taskCount.get());
-        int awaitTaskNum = taskCount.get();
+        Queue<Pair<FutureTask<Object>, Long>> workingTaskQueue = new ArrayBlockingQueue<>(taskCount.get());
+        int awaitTaskNum1 = taskCount.get();
         for (int i = 0; i < taskCount.get(); i++) {
             Runnable worker = workTaskQueue.poll();
             if (Objects.nonNull(worker)) {
                 FutureTask<Object> futureTask = new FutureTask<>(worker, null);
                 threadPoolExecutor.execute(threadFactory.newThread(futureTask));
-                awaitTaskNum--;
-                workingTaskQueue.offer(futureTask);
+                workingTaskQueue.offer(new Pair<>(futureTask, System.currentTimeMillis()));
+                awaitTaskNum1--;
             }
         }
+        int awaitTaskNum = awaitTaskNum1;
         //监控线程执行任务
+        futureTaskExecutionListener(threadFactory, threadPoolExecutor,
+                countDownLatch, workingTaskQueue, awaitTaskNum);
+        threadPoolExecutor.shutdown();
+    }
+
+    private void futureTaskExecutionListener(CustomThreadFactory threadFactory, ThreadPoolExecutor threadPoolExecutor,
+                                             CountDownLatch countDownLatch, Queue<Pair<FutureTask<Object>, Long>> workingTaskQueue, int awaitTaskNum) {
         while (countDownLatch.getCount() != 0) {
-            FutureTask<Object> worktask = workingTaskQueue.poll();
-            if (Objects.requireNonNull(worktask).isDone()) {
+            Pair<FutureTask<Object>, Long> futureTaskLongPair = workingTaskQueue.poll();
+            assert futureTaskLongPair != null;
+            if (System.currentTimeMillis() - futureTaskLongPair.getValue() > maxWait) {
+                //超时了
+                futureTaskLongPair.getKey().cancel(true);
+                System.err.println("线程执行超时,已取消");
+                countDownLatch.countDown();
+                continue;
+            }
+            if (Objects.requireNonNull(futureTaskLongPair.getKey()).isDone()) {
                 countDownLatch.countDown();
                 if (awaitTaskNum > 0) {
                     Runnable worker = workTaskQueue.poll();
                     if (Objects.nonNull(worker)) {
                         FutureTask<Object> futureTask = new FutureTask<>(worker, null);
                         threadPoolExecutor.execute(threadFactory.newThread(futureTask));
-                        workingTaskQueue.offer(futureTask);
+                        workingTaskQueue.offer(futureTaskLongPair);
                     }
                 }
             } else {
-                workingTaskQueue.offer(worktask);
+                workingTaskQueue.offer(futureTaskLongPair);
             }
         }
-        threadPoolExecutor.shutdown();
     }
 }
